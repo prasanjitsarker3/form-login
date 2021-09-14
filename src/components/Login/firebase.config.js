e8e7ae2f9156b89d70d23e47b528141659155ca5
prasanjitsarker3 <sarkerprasanjit379@gmail.com>
2021-09-14 23:49:07 +0600

    login-form

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,12 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBwx7K1CdnQjA2UB2rCZzTlRR0M0WpW2YY",
+    authDomain: "form-login-74002.firebaseapp.com",
+    projectId: "form-login-74002",
+    storageBucket: "form-login-74002.appspot.com",
+    messagingSenderId: "1045293905476",
+    appId: "1:1045293905476:web:ef3b7694d054e2ae846e11"
+  };
+
+  export default firebaseConfig;
